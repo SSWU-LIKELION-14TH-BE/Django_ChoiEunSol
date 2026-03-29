ef7699086db1df4d9c7e7a39f9e97bb18d19801f
eunsol <cindychoi03@naver.com>
2026-03-30 00:55:40 +0900

    feat: 로그인/로그아웃 (Week2)

--- user/views.py
+++ user/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
 from .forms import SignUpForm
 
 def signup_view(request):
@@ -8,8 +8,33 @@ def signup_view(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('signup')
+            return redirect('home')
     else:
         form = SignUpForm()
     
     return render(request, 'signup.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'login.html', {'error': '아이디 또는 비밀번호가 틀렸습니다.'})
+
+    return render(request, 'login.html')
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
+    
+    return redirect('home')
+
+def home_view(request):
+    return render(request, 'home.html')
