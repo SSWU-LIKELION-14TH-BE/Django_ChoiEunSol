a4a530bd91895f0dcfcb0abdaa832f5b29674ab5
eunsol <cindychoi03@naver.com>
2026-04-07 08:01:26 +0900

    feat: 댓글 및 좋아요 기능

--- post/views.py
+++ post/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import PostForm
 from django.contrib.auth.decorators import login_required
-from .models import Post, PostImage
+from .models import Post, PostImage, Comment, CommentLike, PostLike
+from django.views.decorators.http import require_POST
 
 @login_required
 def post_create(request):
@@ -17,7 +18,7 @@ def post_create(request):
 
             for file in files:
                 PostImage.objects.create(post=post, image=file)
-                
+
             return redirect('post_list')
     else:
         form = PostForm()
@@ -40,3 +41,61 @@ def post_delete(request, pk):
         post.delete()
 
     return redirect('post_list')
+
+@login_required
+def comment_create(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    content = request.POST.get('content')
+    parent_id = request.POST.get('parent')
+
+    parent = None
+    if parent_id:
+        parent = Comment.objects.get(id=parent_id)
+
+    Comment.objects.create(
+        post=post,
+        author=request.user,
+        content=content,
+        parent=parent
+    )
+
+    return redirect('post_detail', pk=pk)
+
+@require_POST
+@login_required
+def comment_delete(request, pk, comment_pk):
+    comment = get_object_or_404(Comment, pk=comment_pk)
+
+    if request.user == comment.author:
+        comment.delete()
+
+    return redirect('post_detail', pk=pk)
+
+@login_required
+def post_like_toggle(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    like, created = PostLike.objects.get_or_create(
+        user=request.user,
+        post=post
+    )
+
+    if not created:
+        like.delete()
+
+    return redirect('post_detail', pk=pk)
+
+@login_required
+def comment_like_toggle(request, pk, comment_pk):
+    comment = get_object_or_404(Comment, pk=comment_pk)
+
+    like, created = CommentLike.objects.get_or_create(
+        user=request.user,
+        comment=comment
+    )
+
+    if not created:
+        like.delete()
+
+    return redirect('post_detail', pk=pk)
